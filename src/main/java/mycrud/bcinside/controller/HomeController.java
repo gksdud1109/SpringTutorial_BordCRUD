19d358bff022d8158218806e4cec3c4e3f0e61fc
gksdud1109 <vectorh532@gmail.com>
2025-04-20 15:16:43 +0900

    credential store config

--- src/main/java/mycrud/bcinside/controller/HomeController.java
+++ src/main/java/mycrud/bcinside/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @GetMapping("/main")
-    public String loginPage(@RequestParam(name="logout", required=false) String logout,
+    public String loginMain(@RequestParam(name="logout", required=false) String logout,
                             HttpSession session, Model model) {
 
         if (logout != null) {
