0d7495e3895bd9415cb7b980107981e21cb98817
gksdud1109 <vectorh532@gmail.com>
2025-04-20 15:09:04 +0900

    logout 기능구현

--- src/main/java/mycrud/bcinside/controller/HomeController.java
+++ src/main/java/mycrud/bcinside/controller/HomeController.java
@@ -10,14 +10,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @GetMapping("/main")
-    public String loginPage(HttpSession session,
-                            Model model) {
+    public String loginPage(@RequestParam(name="logout", required=false) String logout,
+                            HttpSession session, Model model) {
+
+        if (logout != null) {
+            session.invalidate();
+            return "redirect:/";
+        }
         String userId = (String) session.getAttribute("userId");
 
         if (userId == null) {
             return "redirect:/";
         }
-
         model.addAttribute("userId", userId);
         return "main";
     }
